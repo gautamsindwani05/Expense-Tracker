382b4ce18e4cde1bbc0ce41bf08206c9ba26084d
Gautam <119624121+gautamsindwani05@users.noreply.github.com>
2023-12-05 00:42:58 +0530

    Add files via upload

---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Home(){
+    return (
+        <div className='flex justify-center items-center text-black text-3xl h-full'>
+            Home
+        </div>
+    );
+}
