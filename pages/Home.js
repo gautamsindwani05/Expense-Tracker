d17b55054da65cc5c17bc026717f02f6448a4d91
Gautam <119624121+gautamsindwani05@users.noreply.github.com>
2023-12-05 00:41:05 +0530

    Delete pages directory

--- pages/Home.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Home(){
-    return (
-        <div className='flex justify-center items-center text-black text-3xl h-full'>
-            Home
-        </div>
-    );
-}
